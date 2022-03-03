088cfd5acb4634e4c4f642e1fb02f70bcec0c264
Jeffv <jefersonalonsov@gmail.com>
2022-03-03 01:00:15 -0500

    Add EntityFramework

---
+++ WSVentas/WSVentas/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+#nullable disable
+
+namespace WSVentas.Models
+{
+    public partial class Cliente
+    {
+        public Cliente()
+        {
+            Venta = new HashSet<Venta>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Venta> Venta { get; set; }
+    }
+}
